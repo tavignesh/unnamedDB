7c1eb8b153b7911c801d6ea514afd6f2dd6913e8
T.A.Vignesh <tavignesh2004@gmail.com>
2021-04-25 15:42:23 +0530

    login procedure -v

--- testing.py
+++ testing.py
@@ -1,16 +1,19 @@
-import pickle
+# import pickle
+#
+# dic = {}
+#
+# try:
+#     dataids = open("dataid.dat", "wb")
+#     pickle.dump(dic, dataids)
+#     dataids.close()
+# except Exception as e:
+#     print(e)
+#     dataids.close()
+#
 
-dic = {}
+a = 9989374283757
+b = 837591273658
 
-try:
-    dataids = open("dataid.dat", "wb")
-    pickle.dump(dic, dataids)
-    dataids.close()
-except Exception as e:
-    print(e)
-    dataids.close()
-
-
-
-
+while True:
+    c = 
 
